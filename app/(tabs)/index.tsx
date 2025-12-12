0de6f9c82f648f808153c7bf6876aae80f44aa9a
Regine Kornbakk <reginekornbakk@gmail.com>
2025-12-12 16:00:58 +0100

    feat: added text and colors for light and dark mode

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,21 @@
 import { Text, View } from "react-native";
+import { Fonts } from '@/constants/theme';
+import { useColorScheme } from 'react-native'
 
 export default function Index() {
+    const colorScheme = useColorScheme()
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: colorScheme === 'dark' ? '#000' : '#fff',
       }}
     >
-      <Text>Welcome to my app!</Text>
-      <Text>Feel free</Text>
+      <Text style={{ fontFamily: Fonts.bold, fontSize: 30, color: colorScheme === 'dark' ? '#fff' : '#000'  }}>Good afternoon!</Text>
+      <Text style={{ fontFamily: Fonts.regular, color: colorScheme === 'dark' ? '#fff' : '#000'   }}>Looking for something tasty?</Text>
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,24 @@
-import { Stack } from "expo-router";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { SplashScreen, Stack } from 'expo-router';
+import { useEffect } from 'react';
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <Stack screenOptions={{ headerShown: false }} />
+  );
 }
