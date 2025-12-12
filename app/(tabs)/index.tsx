e3a6213273cccb3824fd072ee5355d81d3e8612c
Regine Kornbakk <reginekornbakk@gmail.com>
2025-12-12 16:07:53 +0100

    feat: editing styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,16 +6,17 @@ export default function Index() {
     const colorScheme = useColorScheme()
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: colorScheme === 'dark' ? '#000' : '#fff',
-      }}
+<View
+  style={{
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "flex-start",
+    paddingLeft: 25,
+    backgroundColor: colorScheme === 'dark' ? '#000' : '#fff',
+  }}
     >
-      <Text style={{ fontFamily: Fonts.bold, fontSize: 30, color: colorScheme === 'dark' ? '#fff' : '#000'  }}>Good afternoon!</Text>
-      <Text style={{ fontFamily: Fonts.regular, color: colorScheme === 'dark' ? '#fff' : '#000'   }}>Looking for something tasty?</Text>
+      <Text style={{ fontFamily: Fonts.bold, fontSize: 25, color: colorScheme === 'dark' ? '#fff' : '#000', marginBottom: 10 }}>Good afternoon!</Text>
+      <Text style={{ fontFamily: Fonts.regular, color: colorScheme === 'dark' ? '#fff' : '#000'  }}>Looking for something tasty?</Text>
     </View>
   );
 }
